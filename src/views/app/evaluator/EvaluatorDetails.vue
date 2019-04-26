2ac8c617d4958abb5e76ca1750953b9b8e8001b1
CCCI <ccci@Master-iMac.local>
2019-04-26 16:48:50 +0800

    ui evaluate, review

--- src/views/app/evaluator/EvaluatorDetails.vue
+++ src/views/app/evaluator/EvaluatorDetails.vue
@@ -1,9 +1,7 @@
 <template>
   <v-card flat>
-    <v-card-text>
+     <v-card-text>
       <span class="title">Sample Case Name</span>
-      <v-layout row wrap>
-        <v-flex xs12 md12 d-flex>
           <v-tabs
             class="elevation-1 mt-2"
             v-model="tabs"
@@ -17,38 +15,61 @@
             <v-tab ripple>Case Information</v-tab>
             <v-tab-item>
               <v-card flat>
-                <v-card-text class="ml-3 mt-3">
-                  <!-- <v-btn class="mr-3" absolute right dark outline color="primary"> Edit -->
-                <!-- </v-btn> -->
-                  <v-flex xs12 md6 class="font-weight-bold">Docket Number</v-flex>
-                  <v-flex xs12 md12>cmp-apm-2013-130</v-flex>
-                  <br>
-                  <v-flex xs12 md6 class="font-weight-bold">Ref/DTN #</v-flex>
-                  <v-flex xs12 md12>cmp-apm-2013-130</v-flex>
-                  <br>
-                  <v-flex xs12 md6 class="font-weight-bold">Date Received</v-flex>
-                  <v-flex xs12 md12>cmp-apm-2013-130</v-flex>
-                  <br>
-                  <v-flex xs12 md6 class="font-weight-bold">Date Docketed</v-flex>
-                  <v-flex xs12 md12>cmp-apm-2013-130</v-flex>
-                  <br>
-                  <v-flex xs12 md6 class="font-weight-bold">Complainant</v-flex>
-                  <v-flex xs12 md12>cmp-apm-2013-130</v-flex>
-                  <br>
-                  <v-flex xs12 md6 class="font-weight-bold">Complainant Address</v-flex>
-                  <v-flex xs12 md12>cmp-apm-2013-130</v-flex>
-                  <br>
-                  <v-flex xs12 md6 class="font-weight-bold">Respondent</v-flex>
-                  <v-flex xs12 md12>cmp-apm-2013-130</v-flex>
-                  <br>
-                  <v-flex xs12 md6 class="font-weight-bold">Respondent Address</v-flex>
-                  <v-flex xs12 md12>cmp-apm-2013-130</v-flex>
-                  <br>
-                  <v-flex xs12 md6 class="font-weight-bold">Cause of Complaint</v-flex>
-                  <v-flex xs12 md12>cmp-apm-2013-130</v-flex>
-                  <br>
-                  <v-flex xs12 md6 class="font-weight-bold">Product/s Involved (if any)</v-flex>
-                  <v-flex xs12 md12>cmp-apm-2013-130</v-flex>
+                <v-card-text>
+                  <v-container grid-list-xl>
+                  <v-layout row wrap>
+                  <v-flex xs6>
+                  <span class="font-weight-bold">Docket Number</span><br>
+                  <span>cmp-apm-2013-130</span>
+                </v-flex>
+                <br>
+                <v-flex xs6>
+                  <span class="font-weight-bold">Ref/DTN #</span><br>
+                  <span>cmp-apm-2013-130</span>
+                </v-flex>
+                <br>
+                <v-flex xs6>
+                  <span class="font-weight-bold">Date Received</span><br>
+                  <span>cmp-apm-2013-130</span>
+                </v-flex>
+                <br>
+                <v-flex xs6>
+                  <span class="font-weight-bold">Date Docketed</span><br>
+                  <span>cmp-apm-2013-130</span>
+                </v-flex>
+                <br>
+                <v-flex xs6>
+                  <span class="font-weight-bold">Complainant</span><br>
+                  <span>cmp-apm-2013-130</span>
+                </v-flex>
+                <br>
+                <v-flex xs6>
+                  <span class="font-weight-bold">Complainant Address</span><br>
+                  <span>cmp-apm-2013-130</span>
+                </v-flex>
+                <br>
+                <v-flex xs6>
+                  <span class="font-weight-bold">Respondent</span><br>
+                  <span>cmp-apm-2013-130</span>
+                </v-flex>
+                <br>
+                <v-flex xs6>
+                  <span class="font-weight-bold">Respondent</span><br>
+                  <span>cmp-apm-2013-130</span>
+                </v-flex>
+                <br>
+                <v-flex xs6>
+                  <span class="font-weight-bold">Cause of Complaint</span><br>
+                  <span>cmp-apm-2013-130</span>
+                </v-flex>
+                <br>
+                <v-flex xs6>
+                  <span class="font-weight-bold">Product/s Involved (if any)</span><br>
+                  <span>cmp-apm-2013-130</span>
+                </v-flex>
+                <br>
+                </v-layout>
+                  </v-container>
                 </v-card-text>
                 <v-divider></v-divider>
                 <v-card-actions>
@@ -86,59 +107,22 @@
                 </v-card-text>
               </v-card>
             </v-tab-item>
-          </v-tabs>
-        </v-flex>   
+          </v-tabs>   
 
-        <!-- Nav to Evaluate -->
+        <!-- Nav to Review -->
         <v-navigation-drawer permanent right app width="300px" class="elevation-1">
           <v-toolbar flat>
-            <span class="title font-weight-light primary--text" style="text-transform: uppercase">Evaluate</span>
+            <span class="title font-weight-light primary--text" style="text-transform: uppercase">Review</span>
           </v-toolbar>
           <v-card flat>
-            <v-card-text>
-              <v-text-field
-                outline
-                label="1. Laws Violated"
-                name="name"
-                textarea
-                multi-line
-                counter
-              ></v-text-field>
-              <!-- <span class="subheading font-weight-light primary--text">Nature of Violation</span>
-              <v-divider class="mb-3"></v-divider> -->
+            <v-card-text>              
                <v-select
-                label="2. Nature of Violation"
-                :items="natureViolation"
-                v-model="value"
-                autocomplete
-              ></v-select>
-              <!-- <v-select
-                label="Violative Products"
-                :items="violativeProds"
-                v-model="value"
-                autocomplete
-              ></v-select> -->
-              <v-text-field
-                outline
-                label="Remarks"
-                name="name"
-                textarea
-                multi-line
-                counter
-              ></v-text-field>
-               <v-select
-                label="3. Action Taken"
+                label="1. Action Taken"
                 :items="actionTaken"
                 v-model="value"
                 autocomplete
               ></v-select>
-               <!-- <v-select
-                label="If Legal Order"
-                :items="legalOrder"
-                v-model="value"
-                autocomplete
-              ></v-select> -->
-               <v-text-field
+              <v-text-field
                 outline
                 label="Remarks"
                 name="name"
@@ -146,54 +130,9 @@
                 multi-line
                 counter
               ></v-text-field>
-              <v-select
-                label="4. Reason for Remanding"
-                :items="remanding"
-                v-model="value"
-                autocomplete
-              ></v-select>
-               <v-text-field
-                outline
-                label="Remarks"
-                name="name"
-                textarea
-                multi-line
-                counter
-              ></v-text-field>
-              <!-- <v-text-field
-                outline
-                label="3. Decision Category"
-                name="name"
-                textarea
-                multi-line
-                counter
-              ></v-text-field>
-              <v-text-field
-                outline
-                label="4. Penalty Impose"
-                name="name"
-                textarea
-                multi-line
-                counter
-              ></v-text-field>
-              <v-text-field
-                outline
-                label="5. Execution Details / Remarks"
-                name="name"
-                textarea
-                multi-line
-                counter
-              ></v-text-field> -->
               <span class="subheading font-weight-light primary--text">Add Supporting Documents</span>
               <v-divider class="mb-3"></v-divider>
               <uploader class="caption"></uploader>
-              <!-- <v-select
-                label="6. Directive to REU"
-                :items="itemsDirectives"
-                v-model="value"
-                autocomplete
-              ></v-select>-->
-
               <!-- fab button save -->
               <v-tooltip top>
                 <v-btn
@@ -212,11 +151,10 @@
             </v-card-text>
             <v-divider></v-divider>
             <v-card-actions>
-              <v-btn block color="primary">Submit for Review</v-btn>
+              <v-btn block color="primary">Submit for Approval</v-btn>
             </v-card-actions>
           </v-card>
         </v-navigation-drawer>
-      </v-layout>
     </v-card-text>
   </v-card>
 </template>
