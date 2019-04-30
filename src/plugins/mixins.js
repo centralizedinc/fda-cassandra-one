70164d6eb5e5d0728592fdea0e37cb6740f1b95c
Kris Yango <blitzkris24@gmail.com>
2019-04-30 17:15:37 +0800

    admin

--- src/plugins/mixins.js
+++ src/plugins/mixins.js
@@ -21,6 +21,29 @@ export default {
           ]
           return arr[status]
         },
+        caseStatus(status){
+          if (status = 5) {
+            return "Docketed"
+          }
+          else if (status = 0) {
+            return "Evaluated"
+          }
+          else if (status = 2) {
+            return "Approved"
+          }
+          else if (status = 3) {
+            return "Finalized"
+          }
+          else if (status = 4   ) {
+            return "Executed"
+          }
+          //evaluation = 0
+        // review = 1
+        // approval = 2
+        // finalization = 3
+        // execution = 4
+        // creation/docketing = 5
+        },
         formatDate: (date_str, type) => {
           if (!date_str) {
             return "";
@@ -47,14 +70,16 @@ export default {
           this.$store.dispatch("LOGOUT");
           this.$router.push("/login");
         },
-        createActivityDesc(item){
-          console.log("Item#########" + JSON.stringify(item))
-          return "<span class='primary--text'>"+this.formatDate(item.date_created)+"</span> &mdash;  Created Case Docket (Docket Number: "+this.docket.dtn+")  Date Modified "+this.docket.date_modified+""
-        },
-        // createActivityDesc(item){
+        createActivityDesc(item) {
+
+          return "<span class='primary--text'>" + this.formatDate(item.date_created) + "</span> &mdash; "+ this.caseStatus(item.status) +" by " + item.username + "   Remarks: "+ item.comment +"  Type: "+ this.getCaseType(item.stage) +""
+        }
         //   console.log("Item#########" + JSON.stringify(item))
         //   return "<span class='primary--text'>"+this.formatDate(item.date_created)+"</span> &mdash;  Created Case Docket (Docket Number: "+this.docket.dtn+")  Date Modified "+this.formatDate(item.date_modified)+""
-        // },
+        //   date name status type },
+        // date - 
+
+
       }
     })
   }
